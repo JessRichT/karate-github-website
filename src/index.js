ec1b5a7646015490d335b598abed12483249c06d
JessRichT <95279459+JessRichT@users.noreply.github.com>
2023-04-20 22:50:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ import Navbar from './Navbar';
 import Website from './frontPage';
 import Website2 from './membersPage';
 //import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <div className="content">
-      <Switch>
+      <Routes>
         <Route exact path="/">
     <Website />
     </Route>
     <Route path="/members">
       <Website2 />
     </Route>
-    </Switch>
+    </Routes>
     </div>
     </Router>
 );
