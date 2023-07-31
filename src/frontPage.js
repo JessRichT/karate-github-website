950fcf56f98931d674e6d6c7a7b272f1bc81070e
Jessica Richards <jessica.sonja.richards@gmail.com>
2023-07-31 19:14:47 +0100

    images folder

--- src/frontPage.js
+++ src/frontPage.js
@@ -4,10 +4,10 @@ import './index.css';
 import { HashLink } from 'react-router-hash-link';
 import { Link} from "react-router-dom";
 
-import girlKarate from './images/girlKarate.jpg';
-import GroupKarate from './images/GroupKarate.jpg';
-import karateBoy from './images/karateBoy.jpg';
-import KarateLogoColour from './images/KarateLogoColour.png';
+import girlKarate from 'images/girlKarate.jpg';
+import GroupKarate from 'images/GroupKarate.jpg';
+import karateBoy from 'images/karateBoy.jpg';
+import KarateLogoColour from 'images/KarateLogoColour.png';
 //import SteppingStones from './SteppingStones.jpg';
 
 function Website() {
