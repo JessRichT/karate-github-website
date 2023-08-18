8a93e602b69dfe2f64722623f9f71834df38cb44
Jessica Richards <jessica.sonja.richards@gmail.com>
2023-08-18 23:01:37 +0100

    useState changes belt colour info

--- src/membersPage.js
+++ src/membersPage.js
@@ -2,12 +2,16 @@ import React, {useState, useEffect} from 'react';
 import './css/index2.css';
 import { HashLink } from 'react-router-hash-link';
 import { Link} from "react-router-dom";
+import KarateLogoColour from './images/KarateLogoColour.png';
+
+const logo = KarateLogoColour;
 
 //alternative const Website = () => {}
 function Website2() {
 //use state:
-  const[name, setName] = useState("");
-  const[beltColour, setBeltColour] = useState("");
+  //const[name, setName] = useState("");
+  //const[beltColour, setBeltColour] = useState("");
+  const [kata, setKata] = useState("Click on the belt colour to see more information");
 
 ////////JSX (similar to html).  Wrap in <> and </>
   return ( 
@@ -44,24 +48,83 @@ function Website2() {
 
 <section id="Syllabus" class="container_syllabus">
   <h3>Karate syllabus</h3>
-  <p>Here are our Shotokan karate belt colours.  More information about the syllabus is coming soon:</p>
+  <p>Here are our Shotokan karate belt colours:</p>
   <ul>
-    <li style={{ color: 'white' }} >White belt</li>
-    <li style={{ color: 'red' }} >Red belt</li>
-    <li style={{ color: 'orange' }}>Orange belt</li>
-    <li style={{ color: 'yellow' }}>Yellow belt</li>
+    {/*<li style={{ color: 'white' }} >White belt</li>*/}
+    {/*<li style={{ color: 'red' }} >Red belt</li>*/}
+    <li
+        onClick={() => setKata("This is your first belt.")}
+        style={{ color: 'white', cursor: 'pointer' }} >White belt</li>
+
+    <li
+        onClick={() => setKata(
+          "No Katas required.  Basics: Straight punch, stepping punch, reverse punch, upward block, front kick, downward block")}
+        style={{ color: 'red', cursor: 'pointer'}} >Red belt</li>
+
+    {/*<li style={{ color: 'orange' }}>Orange belt</li>*/}
+    <li
+        onClick={() => setKata("Kata: Heian Shodan. Kumite: Gohon Ippon Face and Front Kick")}
+        style={{ color: 'orange', cursor: 'pointer' }} >Orange belt</li>
+
+<li
+        onClick={() => setKata("Kata: Heian Nidan. Kumite: Gohon Ippon Full and Sambon")}
+        style={{ color: 'yellow', cursor: 'pointer' }} >Yellow belt</li>
+
+<li
+        onClick={() => setKata("Kata: Heian Sandan. Kumite: Kihon Ippon Face")}
+        style={{ color: 'green', cursor: 'pointer' }} >Green belt</li>
+
+<li
+        onClick={() => setKata("Kata: Heian Yondan. Kumite: Kihon Ippo Face and Body")}
+        style={{ color: 'blue', cursor: 'pointer' }} >Blue belt</li>
+
+<li
+        onClick={() => setKata("Kata: Heian Godan. Kumite: Kihon Ippon Face, Stomach and Front Kick")}
+        style={{ color: 'purple', cursor: 'pointer' }} >Purple belt</li>
+
+<li
+        onClick={() => setKata("Kata: Tekki Shodan. Kumite: All of Kihon Ippon and Jiyon Ippon Face")}
+        style={{ color: 'brown', cursor: 'pointer' }} >Brown belt</li>
+
+<li
+        onClick={() => setKata("Kata: Bassi-Dai. Kumite: Jiyu Ippon Face and Body")}
+        style={{ color: 'brown', cursor: 'pointer' }} >2nd Kyu belt</li>
+
+<li
+        onClick={() => setKata("Kata: Bassi-dai, Kanku-dai. Kumite: Jiyu Ippon Face Body and Front Kick")}
+        style={{ color: 'brown', cursor: 'pointer' }} >1st Kyu belt</li>
+
+<li
+        onClick={() => setKata("Kata: Enpi and own combination.  Kumite: All of Jiyu Ippon")}
+        style={{ color: 'black', cursor: 'pointer' }} >Black belt</li>
+
+    {/*<li style={{ color: 'yellow' }}>Yellow belt</li>
     <li style={{ color: 'green' }}>Green belt</li>
     <li style={{ color: 'blue' }}>Blue belt</li>
     <li style={{ color: 'purple' }}>Purple belt</li>
     <li style={{ color: 'brown' }}>Brown belt</li>
-    <li style={{ color: 'black' }}>Black belt</li>
+  <li style={{ color: 'black' }}>Black belt</li>*/}
   </ul>  
+
   <p class="note_stripes">N.B. Kids can achieve intermediate grades which are shown with a white stripe on their belt.</p>
-</section>      
+
+</section> 
+
+<section id="beltInfo" class="container_info"> 
+
+<h3>Further Information</h3>
+
+<p class="beltInfoHere">{kata}</p>
+
+<br/>
+
+<p class="note_stripes">Brown belt is the first teaching grade and therefore students who achieve this grade will be offered the opportunity to become assistant instructors to teach lower grades.</p>
+
+  </section>
 
     <section id="Training" class="container_videos">    
-  <h3>Training videos</h3>
-  <p>These videos can help you practise your karate at home between lessons.</p>
+  <h3 class="white">Training videos</h3>
+  <p class="white">These videos can help you practise your karate at home between lessons.</p>
   
   <iframe width="360" height="215" src="https://www.youtube.com/embed/myCc-SJYj0U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
